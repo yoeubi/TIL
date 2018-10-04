e927e9ae32c748e8863ea4026e9f013a9374701f
JungwonSeong <yoeubi28@gmail.com>
2018-10-05 01:51:56 +0900

    node 인강 정리

---
+++ Node/helloNode.js
@@ -0,0 +1,11 @@
+var a = 1;
+var b = 2;
+console.log(a + b);
+console.log('hello node');
+
+// cd .. 상위 폴더로 이동한다 
+// cd [폴더명] 폴더로 이동한다.
+// node helloNode.js 하면 된다.
+
+// 자바스크립트 파일은 js붙는데 확장자를 생략할 수 있다. 
+
